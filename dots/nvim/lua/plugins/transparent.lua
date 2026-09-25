33720b6a105be52d3cbcdf14e7202b01c933ab5b
hq9afk <hieubuiduc1@gmail.com>
2026-09-25 11:54:24 +0700

    update

---
+++ dots/nvim/lua/plugins/transparent.lua
@@ -0,0 +1,10 @@
+-- Use the terminal's background instead of the colorscheme's
+return {
+  {
+    "folke/tokyonight.nvim",
+    opts = {
+      transparent = true,
+      styles = { sidebars = "transparent" },
+    },
+  },
+}
